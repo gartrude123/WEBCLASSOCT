4f74ccd7286d6d35ee9cbbdb6c303552345f6f79
gartrude123 <gatrudekabuwo@gmail.com>
2025-10-27 10:02:16 +0300

    Create JS_SCript.js

---
+++ JS_SCript.js
@@ -0,0 +1,7 @@
+var firstname = "gatrude";
+let lastname = "kabuwo";
+let cw = 35;
+let final_exam = 40;
+let total = cw + final_exam;
+
+alert(final_exam + lastname + " your total mark is " + total);
